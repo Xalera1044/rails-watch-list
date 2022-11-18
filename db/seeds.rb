71660d60f04426721e3280943626f69b8d9400b5
Abbas Nazarov <abbas.nazarov@gmail.com>
2022-11-18 19:14:19 +0400

    almost done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,24 @@ Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conf
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+Movie.create(title: "Black Panther", overview: "After the death of his father, T'Challa returns home to the African nation of Wakanda to take his rightful place as king. When a powerful enemy suddenly reappears,", poster_url: "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg", rating: 7.3)
 
 require "open-uri"
 
 List.destroy_all
 file = URI.open('https://images.unsplash.com/photo-1566013656433-e818796d04f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')
-list = List.new(name: 'fantasy')
+list = List.new(name: 'Fantasy')
+list.photo.attach(io: file, filename: 'drama.png', content_type: 'image/png')
+list.save
+
+
+file = URI.open('https://images.unsplash.com/photo-1518929458119-e5bf444c30f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80')
+list = List.new(name: 'Action')
+list.photo.attach(io: file, filename: 'action.png', content_type: 'image/png')
+list.save
+
+
+file = URI.open('https://images.unsplash.com/photo-1516545595035-b494dd0161e4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')
+list = List.new(name: 'Drama')
 list.photo.attach(io: file, filename: 'drama.png', content_type: 'image/png')
 list.save
